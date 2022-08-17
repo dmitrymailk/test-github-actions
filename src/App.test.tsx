a254d9b0a193e00061bc590ee22fa76f6eb38e35
dmitrymailk <dmitrymail_k@mail.ru>
2022-08-17 21:30:00 +0300

    add correct linter branch and fall test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,3 +7,7 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test("this test always falls", () => {
+  expect(true).toBe(!true);
+});
