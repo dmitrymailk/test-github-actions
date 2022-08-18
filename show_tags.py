7ed48edb3d8bc74d68756841f5db0d32b0236887
dmitrymailk <dmitrymail_k@mail.ru>
2022-08-18 04:02:29 +0300

    fix

--- show_tags.py
+++ show_tags.py
@@ -13,10 +13,6 @@ params = [
         "--new_tag",
         {"dest": "new_tag", "type": str, "default": "1"},
     ),
-    (
-        "--branch",
-        {"dest": "branch", "type": str, "default": "main"},
-    ),
 ]
 for name, param in params:
     parser.add_argument(name, **param)
@@ -27,21 +23,23 @@ args = {arg[0]: arg[1] for arg in args}
 
 actor = args["actor"]
 new_tag = args["new_tag"]
-branch = args["branch"]
-# branch = branch.split("/")[-1]
 
-print(actor, new_tag, branch)
+
+print(
+    actor,
+    new_tag,
+)
 
 last_version = int(new_tag.split(".")[-1])
 if last_version > 0:
     print("ok 1")
     previous_version = last_version - 1
-    tags_command = f"HEAD...rc-0.0.{previous_version}"
+    tags_command = f"HEAD...rc-0.0.{previous_version-1}"
     cmd = [
         "git",
         "log",
-        branch,
-        "--pretty=format:'%h; author: %cn; date: %ci; commit message:%s'",
+        "--oneline",
+        # "--pretty=format:'%h; author: %cn; date: %ci; commit message:%s'",
         tags_command,
     ]
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
